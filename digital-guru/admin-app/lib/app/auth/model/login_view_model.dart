49d0fb21e1a198afcd5637958964af7daa1cf863
Your Name <you@example.com>
2026-03-31 09:41:30 -0500

    update sdk version

--- digital-guru/admin-app/lib/app/auth/model/login_view_model.dart
+++ digital-guru/admin-app/lib/app/auth/model/login_view_model.dart
@@ -1,6 +1,6 @@
 import 'package:digiguru/app/common/constants/route_names.dart';
 import 'package:digiguru/app/common/locator.dart';
-import 'package:digiguru/app/firebase_services/service/analytics_service.dart';
+import 'package:digiguru/app/common/service/analytics_service.dart';
 import 'package:digiguru/app/auth/service/authentication_service.dart';
 import 'package:digiguru/app/common/service/dialog_service.dart';
 import 'package:digiguru/app/common/service/navigation_service.dart';
